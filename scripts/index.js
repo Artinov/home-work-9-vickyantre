31d9792a0508ec6f594067adf332336d7ff248b6
vicky.antre <vicky.antre@gmail.com>
2017-02-25 14:08:44 +0200

    finished homework

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,49 @@
 var ball = document.querySelector(".discoball");
+var step = 0;
 
 function goDisco() {
-	//do your disco magic here !
+		//do your disco magic here !
+		switch (step % 6) {
+			case 0:
+		       	ball.className = "discoball awesome";
+		        break;
+		    case 1:
+		        ball.className = "discoball goes";
+		        break;
+		    case 2:
+		        ball.className = "discoball our";
+		        break;
+		    case 3:
+		        ball.className = "discoball here";
+		        break;
+		    case 4:
+		        ball.className = "discoball disco";
+		        break;
+		    case 5:
+		        ball.className = "discoball ball";
+		        break;
+}
+console.log(step % 6)
+step++;
 }
 
 setInterval(goDisco,300);
+
+var text = document.querySelector(".text");
+var step1 = 0;
+function changeColor() {
+	switch (step1 % 3) {
+		case 0:
+			text.className  = "text red";
+			break;
+		case 1:
+			text.className  = "text green";
+			break;
+		case 2:
+			text.className  = "text white";
+			break;
+	}
+	console.log(step1 % 3);
+	step1++
+}
+setInterval(changeColor,1000)
